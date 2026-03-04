6f1cdb8a9571a758165213dc30223f232b6fe2fc
Victor_Victus <caipoccoa@outlook.com>
2026-03-04 17:56:23 -0500

    introduction

--- 1.introduction/introduction.c
+++ 1.introduction/introduction.c
@@ -2,22 +2,23 @@
 /*The OS use virtualization that means transform a resource into a more general, powerful, and easy to use virtual form of itself*/
 /*The OS is known as a resource manager, because it manage resources (CPU, memory and disk) with many goals in mind*/
 
-#include <stdio.h>
-#include <stdlib.h>//interactua con el sistema operativo malloc() y free() libreria estandar
-#include <sys/time.h>//interactua sistemas tipo linux, mide el tiempo con alta precision, no es libreria estandar
-#include <assert.h>//herramienta de diagnostico de programador, verifica que las cosas sean como deberian ser, libreria estandar
-#include "common.h"//libreria propia del programador
+#include <stdio.h>//input output sistema operativo libreria estandar
+#include <stdlib.h>//memoria sistema operativo malloc() y free() libreria estandar
+#include <unistd.h>//llamadas al sistema operativo libreria sistemas unix
 
 int main (int argc, char *argv[]){
     if (argc != 2) {
-        fprintf(stderr, "usage: cpu <string>\n");//stdio
-        exit(1);//stdlib
+        /*acepta 2 argumentos, el nombre del programa y el argumento util*/
+        fprintf(stderr, "usage: cpu <string>\n");//stdio.h
+        exit(1);//stdlib.h
+        /*devuelve 1 al sistema operativo, es decir sale del programa*/
     }
     char *str = argv[1];
     while (1) {
 
-        Spin(1);//common
-        printf("%s\n", str);//stdio
+        sleep(1);//unistd.h
+        printf("%s\n", str);//stdio.h, %s para strings
     }
     return 0;
+    //to stop the program press ctrl+c
 }
